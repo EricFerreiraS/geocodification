c75654697e6891d0eea8e325c858b38ca30a41f3
EricFerreiraS <eric_santos6@hotmail.com>
2016-10-24 18:12:55 -0200

    modificacao

--- geocodification.py
+++ geocodification.py
@@ -17,18 +17,19 @@ try:
         endereco = str(row[2]).replace(' ', '+')+ ',+' +str(row[3]).replace(' ', '+') + ',+' + str(row[4]).replace(' ', '+') + ',+'+ str(row[5]).replace(' ', '+')
         resp = requests.get(api+endereco)
         if resp.status_code == 200:
-    		value = json.loads(resp.content)
-    		if value['status'] != 'ZERO_RESULTS':
-                    lat = value['results'][0]['geometry']['location']['lat']
-                    lng = value['results'][0]['geometry']['location']['lng']
-                    row.append(lat)
-                    row.append(lng)
-                    writer.writerow(row)
-                else:
-                    writer.writerow(row)
-        print 'Linha {0} processada - {1}'.format(i,datetime.datetime.now())
+            value = json.loads(resp.content)
+            if value['status'] != 'ZERO_RESULTS':
+                lat = value['results'][0]['geometry']['location']['lat']
+                lng = value['results'][0]['geometry']['location']['lng']
+                row.append(lat)
+                row.append(lng)
+                writer.writerow(row)
+            else:
+                writer.writerow(row)
+        #print 'Linha {0} processada - {1}'.format(i,datetime.datetime.now())
         i = i+1
 except csv.Error as e:
-        sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+    sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
 finally:
     f.close()
+    g.close()

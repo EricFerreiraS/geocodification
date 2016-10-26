29a5e826eb2c3b11db5d277547616baafcdd3d3e
EricFerreiraS <eric_santos6@hotmail.com>
2016-10-26 16:08:59 -0200

    inclusão de chaves

--- geocodification.py
+++ geocodification.py
@@ -7,7 +7,7 @@ import time
 
 filename = sys.argv[1]
 api = 'https://maps.googleapis.com/maps/api/geocode/json?address='
-key = ['AIzaSyCJK4dg7NabAMFIwxPKyE2EICiPARah6lw', 'AIzaSyCcaNivsVgF1iSDF5zSwZ3sKJ3JLP7jQVk','AIzaSyAwdsKp74cwS5RJ73HS38oHDtVKw8ZAKgA','AIzaSyAhADEiuwuhs92u1pjC8n1ZTYB5b56yF7c']
+key = ['AIzaSyAhADEiuwuhs92u1pjC8n1ZTYB5b56yF7c','AIzaSyAwdsKp74cwS5RJ73HS38oHDtVKw8ZAKgA','AIzaSyCcaNivsVgF1iSDF5zSwZ3sKJ3JLP7jQVk','AIzaSyCJK4dg7NabAMFIwxPKyE2EICiPARah6lw', 'AIzaSyCcaNivsVgF1iSDF5zSwZ3sKJ3JLP7jQVk','AIzaSyAwdsKp74cwS5RJ73HS38oHDtVKw8ZAKgA','AIzaSyAhADEiuwuhs92u1pjC8n1ZTYB5b56yF7c']
 
 f = open(filename, 'rb')
 g = open(filename+'_changed', 'wb')
@@ -30,7 +30,7 @@ try:
 			elif value['status'] == 'OVER_QUERY_LIMIT':
 				#time.sleep(300)
 				j = j+1
-				print ('Trocando Chave {0}').format(j)
+				print ('Trocando Chave {0}').format(key[j])
 				endereco = str(row[2]).replace(' ', '+')+ ',+' +str(row[3]).replace(' ', '+') + ',+' + str(row[4]).replace(' ', '+') + ',+'+ str(row[5]).replace(' ', '+')
 				resp = requests.get(api+endereco+'&key='+key[j])
 				value = json.loads(resp.content)
